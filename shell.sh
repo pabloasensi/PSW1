53a07d7b2bb787390973eff594be2a11e28b6e6a
pabloasensi <pabsensi@opendeusto.es>
2018-02-13 00:02:05 +0100

    hanges to shell

--- shell.sh
+++ shell.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 if [[ $1 = "-c" ]]; then
 	java -jar jplag.jar -l $2 -s $3 ; python batchparse.py;
-	echo -n "Would you like to remove the results folder? (Y/N) ";
-	read answer
-	if echo "$answer" | grep -iq "^y" ; then
-	rm -rf result;
-	fi
 	
 elif [[ $1 = "-s" ]]; then
 	cp -r $3 ./aux/ ;
@@ -17,12 +12,5 @@ elif [[ $1 = "-s" ]]; then
 	if echo "$answer" | grep -iq "^y" ; then
 	python codedisplay.py;
 	fi
-
-	echo -n "Would you like to remove the results folder? (Y/N) ";
-	read answer
-	if echo "$answer" | grep -iq "^y" ; then
-	rm -rf result;
-	fi
-
 	rm -rf aux;
 fi
