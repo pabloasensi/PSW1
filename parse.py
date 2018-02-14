b59fdb2e6f0e30e3f3bae66906784a7884eb83b5
pabloasensi <pabsensi@opendeusto.es>
2018-02-14 17:52:36 +0100

    Update parse.py

--- parse.py
+++ parse.py
@@ -3,19 +3,22 @@ from HTMLParser import HTMLParser
 # create a subclass and override the handler methods
 class MyHTMLParser(HTMLParser):
 	neededata = False
+	counter = 0
 
 	def handle_starttag(self, tag, attrs):
-		if tag == 'td' or tag == 'th':
-			self.neededata = True
+		if tag == 'th':
+			self.counter += 1
 			# print "Encountered a start tag:", tag
 
 
 	def handle_data(self, data):
-		if self.neededata and data != '-':
+		if self.counter == 2:
 			print data
-			self.neededata = False
+			print " <->"
+		if self.counter == 3:
+			print data
 
 parser = MyHTMLParser()
 f = open('./result/match0-top.html', 'r')
-print '/n'
+print '\n'
 parser.feed(f.read())
